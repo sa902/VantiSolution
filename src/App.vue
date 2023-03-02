47fb73c8276bdcf7d407ba31e4a5414e782def28
sa902 <38661702+sa902@users.noreply.github.com>
2023-03-02 13:07:32 +0000

    Task/2 finished (#1)
    * added vue router 3.6
    
    * handles weird  cases in the url
    
    * handles spaces in floor 1 and floor 2
    
    * made sidebar buttons router links as opposed to changing the table
    
    * tested - users can navigate around the app, filter from navbar and they can copy link to other user
    
    * cant believe i undid that commit
    
    * allows default floor
    
    * fixed bug I reintroduced :hankey:
    
    * variable change

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,17 @@
       <div class="d-flex text-h6 justify-center py-5">Available Floors</div>
       <v-divider />
       <v-list nav>
-        <v-list-item v-for="floor in floors" :key="floor" link>
-          <v-list-item-content @click="handleFloorsSelection(floor)">
+        <v-list-item
+          v-for="floor in floors"
+          :key="floor"
+          link
+          :to="{
+            name: 'Occupancy',
+            params: { floor: parseFloorForURL(floor) },
+          }"
+          @click="handleFloorsSelection(floor)"
+        >
+          <v-list-item-content>
             <v-list-item-title class="d-flex justify-center">
               {{ floor }}
             </v-list-item-title>
@@ -18,24 +27,22 @@
       <v-toolbar-title> Vanti Front End Dev Assessment </v-toolbar-title>
     </v-app-bar>
     <v-main>
-      <v-container>
-        <v-card>
-          <occupancy-table />
-        </v-card>
-      </v-container>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import { computed, ref } from "vue";
-import OccupancyTable from "@/occupancy/OccupancyTable.vue";
 import { useRecords } from "@/occupancy/records";
 
 const records = useRecords();
 const drawer = ref(true);
 const handleFloorsSelection = (floor) => {
   records.selectedFloor = floor;
+};
+const parseFloorForURL = (floor) => {
+  return floor.replace(/\s/g, "");
 };
 const floors = computed(() => {
   let calculatedFloors = [...records.floors];
