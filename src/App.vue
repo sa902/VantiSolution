84c25ce61be48ace55549b06b810c12a746e7f0b
sam <samanderson902@gmail.com>
2023-03-02 09:23:55 +0000

    added side bar'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <template>
   <v-app class="app-root">
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+    >
+      <div class="d-flex text-h6 justify-center pt-4 pb-4">
+        Available Floors
+      </div>
+      <v-divider>
+
+      </v-divider>
+
+      <v-list
+          dense
+          nav
+      >
+        <v-list-item
+            v-for="floor in floors"
+            :key="floor"
+            link
+        >
+          <v-list-item-content @click="takeContent(floor)">
+            <v-list-item-title>{{ floor }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
     <v-app-bar app color="primary">
-      Vanti Front End Dev Assessment
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        Vanti Front End Dev Assessment
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
@@ -15,5 +45,16 @@
 </template>
 
 <script setup>
+import {computed, ref} from "vue";
+import {useRecords} from '@/occupancy/records';
 import OccupancyTable from '@/occupancy/OccupancyTable.vue';
+const records = useRecords();
+const drawer = ref(true);
+const takeContent = (content) => {
+  console.log(`this si the ${content}`)
+}
+const floors = computed(() => {
+  console.log(`this is records ${records}`)
+  return records.floors
+})
 </script>
